a8e6314cbb9d45974cff86db6d3b93e20c9a5f22
Great Bird <greatbabybird@gmail.com>
2023-04-28 19:24:55 -0700

    Add ShapeBase.lua

--- src/shared/shapes/area/ShapeBase.lua
+++ src/shared/shapes/area/ShapeBase.lua
@@ -1,15 +1,14 @@
-local ShapeBaseInterface = {}
 local ShapeBase = {}
 ShapeBase.__index = ShapeBase
 
-function ShapeBaseInterface.new()
+function ShapeBase.new()
     return setmetatable({}, ShapeBase)
 end
 
-export type ShapeBase = typeof(ShapeBaseInterface.new())
+export type ShapeBase = typeof(ShapeBase.new())
 
 function ShapeBase.GetArea(self: ShapeBase)
     return 0
 end
 
-return ShapeBaseInterface
+return ShapeBase
